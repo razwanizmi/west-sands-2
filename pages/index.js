ace1030b748b7cfa48e97aadb2c4ef6f02430346
Razwan Rashidi <razwan.izmi@gmail.com>
2018-11-09 10:08:12 +0800

    Complete reviews

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Navigation from "../components/shared/Navigation";
 import Gallery from "../components/room/Gallery";
 import Summary from "../components/room/Summary";
 import Highlights from "../components/room/Highlights";
+import Reviews from "../components/room/Reviews";
 
 const Index = () => (
   <Layout>
@@ -10,6 +11,7 @@ const Index = () => (
     <Gallery />
     <Summary />
     <Highlights />
+    <Reviews />
   </Layout>
 );
 
